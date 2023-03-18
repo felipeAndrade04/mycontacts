6713c95f06f3f8324a3254cc3d2893c8040cab43
Felipe Andrade <flpandrade2011@gmail.com>
2023-03-18 13:21:22 -0300

    build: configure eslint and editorConfig

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,9 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
 app.get('/', (request, response) => {
-  response.send('Hello')
-})
+  response.send('Hello');
+});
 
-app.listen(3000, () => console.log('🚀 Server started at http://localhost:3000'))
+app.listen(3000, () => console.log('🚀 Server started at http://localhost:3000'));
