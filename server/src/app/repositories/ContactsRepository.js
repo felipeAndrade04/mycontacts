682449b2d625267eb564e9da010b14c8c8451ea0
Felipe Andrade <flpandrade2011@gmail.com>
2023-03-19 10:39:59 -0300

    feat: add delete and findById methods

--- server/src/app/repositories/ContactsRepository.js
+++ server/src/app/repositories/ContactsRepository.js
@@ -1,16 +1,37 @@
 const { uuid } = require('uuidv4');
 
-const contacts = [{
-  id: uuid(),
-  name: 'Felipe',
-  email: 'felipe@gmail.com',
-  category_id: uuid(),
-}];
+let contacts = [
+  {
+    id: uuid(),
+    name: 'Felipe',
+    email: 'felipe@gmail.com',
+    category_id: uuid(),
+  },
+  {
+    id: uuid(),
+    name: 'Felipe 1',
+    email: 'felipe1@gmail.com',
+    category_id: uuid(),
+  },
+];
 
 class ContactController {
   findAll() {
     return new Promise((resolve) => {
       resolve(contacts);
+    });
+  }
+
+  findById(id) {
+    return new Promise((resolve) => {
+      resolve(contacts.find((contact) => contact.id === id));
+    });
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
     });
   }
 }
