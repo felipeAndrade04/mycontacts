a55e494e7a778aed5e8734f696d37d8c6469c15f
Felipe Andrade <flpandrade2011@gmail.com>
2023-05-28 17:20:50 -0300

    feat: add select component

---
+++ client/src/components/Select.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export default styled.select`
+  width: 100%;
+  height: 52px;
+  background: #ffffff;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  border-radius: 4px;
+  border: 2px solid #ffffff;
+  outline: none;
+  padding: 0 16px;
+  font-size: 16px;
+  transition: border-color 0.2s ease-in;
+
+  &:focus {
+    border: 2px solid ${({ theme }) => theme.colors.primary.main};
+  }
+`;
