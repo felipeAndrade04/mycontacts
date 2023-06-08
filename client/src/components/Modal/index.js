55347e7362a4f478b57bab97d1abab65e8ae3cf6
Felipe Andrade <flpandrade2011@gmail.com>
2023-06-08 14:42:27 -0300

    feat: add react portal in modal

--- client/src/components/Modal/index.js
+++ client/src/components/Modal/index.js
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
 import { Container, Footer, Overlay } from './styles';
 import Button from '../Button';
 
 export default function Modal({ danger }) {
-  return (
+  return ReactDOM.createPortal(
     <Overlay>
       <Container danger={danger}>
         <h1>Título do modal</h1>
@@ -18,7 +19,8 @@ export default function Modal({ danger }) {
           </Button>
         </Footer>
       </Container>
-    </Overlay>
+    </Overlay>,
+    document.getElementById('modal-root')
   );
 }
 
