17989248fb20b0e6cf5a2e2c0755c2d00a25a143
Felipe Andrade <flpandrade2011@gmail.com>
2023-10-17 19:25:44 -0300

    feat: add EventManager

---
+++ client/src/lib/EventManager.js
@@ -0,0 +1,39 @@
+export default class EventManager {
+  constructor() {
+    this.listeners = {};
+  }
+
+  addListener(event, listener) {
+    if (!this.listeners[event]) {
+      this.listeners[event] = [];
+    }
+
+    this.listeners[event].push(listener);
+  }
+
+  emit(event, payload) {
+    if (!this.listeners[event]) {
+      return;
+    }
+
+    this.listeners[event].forEach((listener) => {
+      listener(payload);
+    });
+  }
+
+  removeListener(event, listenerToRemove) {
+    const listeners = this.listeners[event];
+
+    if (!listeners) {
+      return;
+    }
+
+    const filteredListeners = listeners.filter(
+      (listener) => listener !== listenerToRemove
+    );
+
+    this.listeners[event] = filteredListeners;
+  }
+}
+
+const toastEventManager = new EventManager();
