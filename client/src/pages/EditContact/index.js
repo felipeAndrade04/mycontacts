9b8259c1f33e6c1a2a5c8219ed23837ec0e40dfa
Felipe Andrade <flpandrade2011@gmail.com>
2023-10-25 20:28:14 -0300

    feat: get contant in edit page

--- client/src/pages/EditContact/index.js
+++ client/src/pages/EditContact/index.js
@@ -1,11 +1,44 @@
+import { useParams, useHistory } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import ContactForm from '../../components/ContactForm';
 import PageHeader from '../../components/PageHeader';
+import ContactsService from '../../services/ContactsService';
+import Loader from '../../components/Loader';
+import toast from '../../utils/toast';
 
-export default function Home() {
+export default function EditContact() {
+  const [isLoading, setIsLoading] = useState(true);
+  const history = useHistory();
+  const { id } = useParams();
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contactData = await ContactsService.getContactById(id);
+
+        console.log(contactData);
+        setIsLoading(false);
+      } catch {
+        history.push('/');
+        toast({
+          type: 'danger',
+          text: 'Contato não encontrado!',
+        });
+      }
+    }
+
+    loadContact();
+  }, [id, history]);
+
+  function handleSubmit() {
+    //
+  }
+
   return (
     <>
+      <Loader isLoading={isLoading} />
       <PageHeader title="Editar Felipe Andrade" />
-      <ContactForm buttonLabel="Salvar alterações" />
+      <ContactForm buttonLabel="Salvar alterações" onSubmit={handleSubmit} />
     </>
   );
 }
